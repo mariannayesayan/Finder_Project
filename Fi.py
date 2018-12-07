88d6572d84a85f3bddffe867b27190a76657851c
AMPComp <marianna_yesayan@edu.aua.am>
2018-12-07 13:14:20 +0400

    create fi file

--- Fi.py
+++ Fi.py
@@ -27,9 +27,19 @@ class Finder:
         degree = raw_input("please enter value for degree or enter "" if there is no value")
         jobtitle = raw_input("please enter value for jobtitle or enter "" if there is no value")
         organization = raw_input("please enter value for organization or enter "" if there is no value")
-        user = User(firstname, lastname, degree, jobtitle, organization)
-        return user
+
+        return findUser(firstname, lastname, degree, jobtitle, organization)
 
     def displayuserinfo(self, user):
-        print "firsname: ", user.firstname, "lastname: ", user.lastname, "degree: ", user.degree,\
+        print "firstame: ", user.firstname, "lastname: ", user.lastname, "degree: ", user.degree,\
             "jobtitle: ", user.jobtitle, "organization: ", user.organization
+
+
+
+
+def main():
+    finder = Finder ()
+    u1 = User("Levon", "Mnacakanyan","PHD", "assistant", "organization2")
+    u2 = User ("Lusine", "Tovmasyan", "Masters", "Lecturer", "organization1",)
+
+
