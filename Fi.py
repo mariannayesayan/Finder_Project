028056820e8caed610fa2f4de19f35e95505ddff
AMPComp <marianna_yesayan@edu.aua.am>
2018-12-07 12:59:37 +0400

    create fi file

--- Fi.py
+++ Fi.py
@@ -1,14 +1,35 @@
-class Person:
-    def __init__(self, name, lastname, age, ID, mail):
-        self.name = name
+class User:
+    def __init__(self, firstname, lastname, degree, jobtitle, organization):
+        self.firstname = firstname
         self.lastname = lastname
-        self.age = age
-        self.ID = ID
-        self.mail = mail
+        self.degree = degree
+        self.jobtitle = jobtitle
+        self.organization = organization
+        self.next = None
 
-def main():
-    p1 = Person("Marianna", "Yessayan", "18", "12345678", "mail")
-    print "Name:", p1.name, " Lastname:", p1.lastname, " Age:",  p1.age, " ID:",  p1.ID, p1.mail, "mail:"
-    p2 = Person("Satenik", "Tovmasyan", "20", "34129876")
-    print "Name:", p2.name, "Lastname:", p2.lastname, "Age:", p2.age, "ID:", p2.ID, p.mail, "mail:"
-main()
+class Finder:
+    def __init__(self):
+        self.users = None
+
+    def append(self,user):
+        if self.users is None:
+            self.users = user
+        else:
+            temp = self.users
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = user
+
+    def askfindinfo(self):
+        print "here are the keywords: firstname, lastname, degree, jobtitle, organization"
+        firstname = raw_input("please enter value for firstname or enter "" if there is no value")
+        lastname = raw_input("please enter value for lastname or enter "" if there is no value")
+        degree = raw_input("please enter value for degree or enter "" if there is no value")
+        jobtitle = raw_input("please enter value for jobtitle or enter "" if there is no value")
+        organization = raw_input("please enter value for organization or enter "" if there is no value")
+        user = User(firstname, lastname, degree, jobtitle, organization)
+        return user
+
+    def displayuserinfo(self, user):
+        print "firsname: ", user.firstname, "lastname: ", user.lastname, "degree: ", user.degree,\
+            "jobtitle: ", user.jobtitle, "organization: ", user.organization
